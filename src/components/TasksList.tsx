cad1d1c31a721a082f23b203a05f1f3ce8442d8d
Juan Jacobo Jaramillo <jjjaramilloar@unal.edu.co>
2022-02-17 11:58:43 -0500

    Added Component to show the tasks list

---
+++ src/components/TasksList.tsx
@@ -0,0 +1,16 @@
+import TodoModel from "../TodoModel";
+import Todo from "./Todo";
+interface Props {
+  todos: TodoModel[];
+}
+const TasksList: React.VFC<Props> = ({ todos }: Props) => {
+  return (
+    <div className="mx-auto">
+      {todos.map(({ text, isDone, id }) => {
+        return <Todo key={id} text={text} isDone={isDone} />;
+      })}
+    </div>
+  );
+};
+
+export default TasksList;
